73c2aae74fb61658f13146abc90653ca6264775c
Viraj Samarasinghe <123957527+virajsamarasinghe@users.noreply.github.com>
2024-08-22 17:05:10 +0530

    implement method on going
    not done check it

---
+++ src/main/java/com/moviefix/service/FileService.java
@@ -0,0 +1,14 @@
+package com.moviefix.service;
+
+import org.springframework.web.multipart.MultipartFile;
+import java.io.IOException;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+
+public interface FileService {
+
+    String uploadFile(String path, MultipartFile file) throws IOException;
+
+    InputStream getResourceFile(String path, String name) throws FileNotFoundException;
+
+}
---
+++ src/main/java/com/moviefix/service/FileServiceImpl.java
@@ -0,0 +1,12 @@
+package com.moviefix.service;
+
+
+
+
+
+public class FileServiceImpl implements FileService{
+
+
+
+    
+}
